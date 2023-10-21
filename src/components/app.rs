bdbaa1507646fc29e4b5bf39eff723bfa8cc8bb9
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-10-21 02:35:49 -0400

    Update app.rs
    Updated App component to test out the Steam Web API implementation.

--- src/components/app.rs
+++ src/components/app.rs
@@ -2,22 +2,42 @@
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
 use ::dioxus::prelude::*;
-use crate::io::writeAuth_RetroAchievements;
+use crate::io::writeAuth_Steam;
 use crate::platforms::retroachievements::AuthObject;
+use crate::platforms::steam::{AuthData, SteamApi};
 
 #[derive(PartialEq, Props)]
 pub struct AppProps
 {
-	pub auth: Option<AuthObject>,
+	pub retroAuth: Option<AuthObject>,
+	pub steamAuth: Option<AuthData>,
 }
 
 /**
 The root component of the application.
 */
-pub fn App(cx: Scope<AppProps>) -> Element
+pub fn App<'a>(cx: Scope<AppProps>) -> Element
 {
-	let username = use_state(cx, || match cx.props.auth.is_some() { true => cx.props.auth.as_ref().unwrap().username.to_owned(), _ => String::new() });
-	let apiKey = use_state(cx, || match cx.props.auth.is_some() { true => cx.props.auth.as_ref().unwrap().key.to_owned(), _ => String::new() });
+	let api = use_ref(cx, || SteamApi::default());
+	let id = use_state(cx, || match cx.props.steamAuth.is_some() { true => cx.props.steamAuth.as_ref().unwrap().id.to_owned(), _ => String::new() });
+	let apiKey = use_state(cx, || match cx.props.steamAuth.is_some() { true => cx.props.steamAuth.as_ref().unwrap().key.to_owned(), _ => String::new() });
+	let shouldRunFuture = use_state(cx, || true);
+	
+	let apiClone = api.read().clone();
+	to_owned![shouldRunFuture];
+	let future = use_future(cx, (), |_| async move
+	{
+		println!("{:?}", apiClone.auth.clone());
+		let response = apiClone.getPlayerSummaries().await;
+		let value = match response
+		{
+			Ok(summary) => format!("{:?}", summary),
+			Err(e) => format!("{:?}", e),
+		};
+		
+		shouldRunFuture.set(false);
+		return value;
+	});
 	
 	return cx.render(rsx!
 	{
@@ -25,11 +45,11 @@ pub fn App(cx: Scope<AppProps>) -> Element
 		hr {}
 		div
 		{
-			h3 { "Update Retro Achievements Auth Info" }
+			h3 { "Update Steam Auth Info" }
 			div
 			{
-				label { r#for: "authUsername", "Username:" }
-				input { name: "authUsername", r#type: "text", value: "{username}", onchange: move |e| username.set(e.value.clone()) }
+				label { r#for: "authId", "Steam ID:" }
+				input { name: "authId", r#type: "text", value: "{id}", onchange: move |e| id.set(e.value.clone()) }
 			}
 			div
 			{
@@ -39,11 +59,8 @@ pub fn App(cx: Scope<AppProps>) -> Element
 			button
 			{
 				onclick: move |_| {
-					let auth = AuthObject { username: username.to_string(), key: apiKey.to_string() };
-					let _result = writeAuth_RetroAchievements(auth);
-					
-					username.set(String::new());
-					apiKey.set(String::new());
+					let auth = AuthData { id: id.to_string(), key: apiKey.to_string() };
+					let _result = writeAuth_Steam(auth);
 				},
 				"Update"
 			}
@@ -51,7 +68,25 @@ pub fn App(cx: Scope<AppProps>) -> Element
 		hr {}
 		div
 		{
-			h3 { "Retro Achievements" }
+			h3 { "Steam" }
+			button
+			{
+				onclick: move |_|
+				{
+					if let Some(auth) = &cx.props.steamAuth
+					{
+						api.write().auth = auth.to_owned();
+					}
+					
+					future.restart();
+					
+					if let Some(summary) = future.value()
+					{
+						println!("{}", summary);
+					}
+				},
+				"Run GetPlayerSummaries Request"
+			}
 		}
 	});
 }
