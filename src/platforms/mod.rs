78eabe6867dbe6c1b07b52324b96f4ad867478cd
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-10-21 15:30:01 -0400

    Update mod.rs
    Added Platform enumeration.

--- src/platforms/mod.rs
+++ src/platforms/mod.rs
@@ -1,5 +1,14 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+use ::serde::{Deserialize, Serialize};
+
 pub mod retroachievements;
 pub mod steam;
+
+#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, PartialOrd, Ord, Serialize)]
+pub enum Platform
+{
+	RetroAchievements,
+	Steam,
+}
