4f8aa71dbccc30630c51f3a9afff9d9e064bdad6
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-10-23 18:51:16 -0400

    Update app.rs
    Greatly simplified the usage of the APIs; Instead of relying on the use_future hook, which always runs at least once right away, utilize cx.spawn() in the event handlers to generate a new future on demand.

--- src/components/app.rs
+++ src/components/app.rs
@@ -3,8 +3,7 @@
 
 use ::dioxus::prelude::*;
 use ::fermi::use_atom_ref;
-use crate::platforms::steam::SteamApi;
-use crate::state::{User, RetroAchievementsAuth, SteamAuth, loadUserData, saveUserData};
+use crate::state::{UserData, RetroAchievements, Steam};
 
 /**
 The root component of the application.
@@ -13,54 +12,19 @@ pub fn App(cx: Scope) -> Element
 {
 	fermi::use_init_atom_root(cx);
 	
-	let _retroAuth = use_atom_ref(cx, &RetroAchievementsAuth);
-	let steamAuth = use_atom_ref(cx, &SteamAuth);
-	let user = use_atom_ref(cx, &User);
+	let _retroA = use_atom_ref(cx, &RetroAchievements);
+	let steam = use_atom_ref(cx, &Steam);
+	let userData = use_atom_ref(cx, &UserData);
 	
-	let api = use_ref(cx, || SteamApi::new(steamAuth.read().clone()).unwrap());
-	let id = use_state(cx, || steamAuth.read().id.to_owned());
-	let apiKey = use_state(cx, || steamAuth.read().key.to_owned());
-	
-	let api1 = api.clone();
-	let user1 = user.clone();
-	let playerSummaries = use_future(cx, (), |_| async move
-	{
-		match api1.read().getPlayerSummaries().await
-		{
-			Ok(payload) => {
-				println!("{:?}", payload);
-				match payload.response.players.first()
-				{
-					Some(profile) => {
-						let mut userRef = user1.write();
-						userRef.steam.name = profile.personaname.clone();
-						userRef.steam.id = profile.steamid.clone();
-					},
-					None => {},
-				}
-			},
-			Err(e) => println!("Failed to retrieve player summaries! {:?}", e),
-		}
-	});
-	
-	let api2 = api.clone();
-	let user2 = user.clone();
-	let ownedGames = use_future(cx, (), |_| async move
-	{
-		match api2.read().getOwnedGames().await
-		{
-			Ok(payload) => {
-				println!("game count: {}", payload.response.game_count);
-				user2.write().processSteamGames(payload.response.games.clone());
-			},
-			Err(e) => println!("Failed to retrieve owned games! {:?}", e),
-		}
-	});
+	let id = use_state(cx, || String::new());
+	let apiKey = use_state(cx, || String::new());
 	
 	return cx.render(rsx!
 	{
 		h1 { "Local Achievements" }
+		
 		hr {}
+		
 		div
 		{
 			h3 { "Update Steam Auth Info" }
@@ -77,15 +41,17 @@ pub fn App(cx: Scope) -> Element
 			button
 			{
 				onclick: move |_| {
-					let mut steamRef = steamAuth.write();
-					steamRef.id = id.to_string();
-					steamRef.key = apiKey.to_string();
-					api.write().auth = steamRef.clone();
+					
+					let mut steamRef = steam.write();
+					steamRef.auth.id = id.to_string();
+					steamRef.auth.key = apiKey.to_string();
 				},
 				"Update"
 			}
 		}
+		
 		hr {}
+		
 		div
 		{
 			h3 { "Steam" }
@@ -96,39 +62,8 @@ pub fn App(cx: Scope) -> Element
 				{
 					button
 					{
-						onclick: move |_| println!("Steam Info: {:?}", user.read().steam),
+						onclick: move |_| println!("Steam Info: {:?}", userData.read().steam),
 						"Print Steam Info"
-					}
-				}
-				div
-				{
-					button
-					{
-						onclick: move |_| {
-							match loadUserData()
-							{
-								Ok(data) => {
-									println!("Profile loaded!");
-									*user.write() = data;
-								},
-								Err(_) => println!("Failed to load profile!"),
-							}
-						},
-						"Load Profile Data"
-					}
-				}
-				div
-				{
-					button
-					{
-						onclick: move |_| {
-							match saveUserData(user.read().clone())
-							{
-								Ok(_) => println!("Profile saved!"),
-								Err(_) => println!("Failed to save profile!"),
-							}
-						},
-						"Save Profile Data"
 					}
 				}
 			}
@@ -137,14 +72,28 @@ pub fn App(cx: Scope) -> Element
 			{
 				button
 				{
-					onclick: move |_|
+					onclick: move |_| cx.spawn(
 					{
-						if api.read().auth.validate()
-						{
-							playerSummaries.cancel(cx);
-							playerSummaries.restart();
+						to_owned![steam, userData];
+						async move {
+							if let Ok(payload) = steam.read().getPlayerSummaries().await
+							{
+								println!("{:?}", payload);
+								if let Some(profile) = payload.response.players.first()
+								{
+									userData.write().steam.update(
+										profile.steamid.to_owned(),
+										profile.personaname.to_owned(),
+										match profile.avatarhash.is_empty()
+										{
+											true => None,
+											false => Some(profile.avatarhash.to_owned()),
+										}
+									);
+								}
+							}
 						}
-					},
+					}),
 					"Get Player Summaries"
 				}
 			}
@@ -153,13 +102,17 @@ pub fn App(cx: Scope) -> Element
 			{
 				button
 				{
-					onclick: move |_| {
-						if api.read().auth.validate()
-						{
-							ownedGames.cancel(cx);
-							ownedGames.restart();
+					onclick: move |_| cx.spawn(
+					{
+						to_owned![steam, userData];
+						async move {
+							if let Ok(payload) = steam.read().getOwnedGames().await
+							{
+								println!("Game count: {}", payload.response.game_count);
+								userData.write().processSteamGames(payload.response.games);
+							}
 						}
-					},
+					}),
 					"Get Owned Games"
 				}
 			}
