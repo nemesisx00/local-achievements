0ada77f8f0c03f6b285c550305104a9eb0427835
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-10-22 20:22:18 -0400

    Update state.rs
    Added some context to the user data load/save errors.
    
    Normalized the read/write auth functions to use dir for the directory variable and path for the full file path.

--- src/state.rs
+++ src/state.rs
@@ -24,9 +24,11 @@ pub fn loadUserData() -> Result<User>
 	if let Some(dir) = getDataDir(false)
 	{
 		let path = Path::new(dir.as_str()).join(User::Filename);
-		let file = File::open(&path.as_path())?;
+		let file = File::open(&path.as_path())
+			.context(format!("Failed opening file at: '{}'", path.as_path().to_str().unwrap()))?;
 		let buffer = BufReader::new(file);
-		let instance: User = serde_json::from_reader(buffer)?;
+		let instance: User = serde_json::from_reader(buffer)
+			.context("Failed parsing User data file as JSON!")?;
 		return Ok(instance);
 	}
 	
@@ -41,9 +43,11 @@ pub fn saveUserData(user: User) -> Result<()>
 	if let Some(dir) = getDataDir(true)
 	{
 		let path = Path::new(dir.as_str()).join(User::Filename);
-		let file = File::create(&path.as_path())?;
+		let file = File::create(&path.as_path())
+			.context(format!("Failed creating or truncating the file at: '{}'", path.as_path().to_str().unwrap()))?;
 		let buffer = BufWriter::new(file);
-		serde_json::to_writer(buffer, &user)?;
+		serde_json::to_writer(buffer, &user)
+			.context("serde_json failed writing User data to BufWriter")?;
 		return Ok(());
 	}
 	
@@ -55,12 +59,12 @@ Read the RetroAchievements API authorization data from file.
 */
 pub fn readAuth_RetroAchievements() -> Result<AuthObject>
 {
-	if let Some(path) = getConfigDir(true)
+	if let Some(dir) = getConfigDir(true)
 	{
-		let finalPath = Path::new(path.as_str())
+		let path = Path::new(dir.as_str())
 			.join(AuthObject::FileName);
-		let file = File::open(finalPath.as_path())
-			.context(format!("Failed opening file at: '{}'", finalPath.as_path().to_str().unwrap()))?;
+		let file = File::open(path.as_path())
+			.context(format!("Failed opening file at: '{}'", path.as_path().to_str().unwrap()))?;
 		let reader = BufReader::new(file);
 		let instance = serde_json::from_reader(reader)
 			.context("Failed parsing RetroAchievements AuthObject file as JSON")?;
@@ -95,12 +99,12 @@ Write the RetroAchievements API authorization data to file.
 */
 pub fn writeAuth_RetroAchievements(auth: AuthObject) -> Result<()>
 {
-	if let Some(path) = getConfigDir(true)
+	if let Some(dir) = getConfigDir(true)
 	{
-		let finalPath = Path::new(path.as_str())
+		let path = Path::new(dir.as_str())
 			.join(AuthObject::FileName);
-		let file = File::create(finalPath.as_path())
-			.context(format!("Failed creating or truncating the file at: '{}'", finalPath.as_path().to_str().unwrap()))?;
+		let file = File::create(path.as_path())
+			.context(format!("Failed creating or truncating the file at: '{}'", path.as_path().to_str().unwrap()))?;
 		let writer = BufWriter::new(file);
 		serde_json::to_writer(writer, &auth)
 			.context("serde_json failed writing RetroAchievements AuthObject to BufWriter")?;
