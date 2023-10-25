8a96e12e5447a634cafb137088f1bd3e3ffba2c9
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-10-25 02:39:37 -0400

    Update io.rs
    Added function cacheImage for saving images to the Cache directory, organized by platform.

--- src/io.rs
+++ src/io.rs
@@ -1,12 +1,41 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, File};
+use std::io::{ErrorKind, Write};
+use std::path::Path;
+use ::anyhow::{Context, Result};
 use ::directories::ProjectDirs;
+use crate::error;
 
 const Application: &str = "local-achievements";
 const Organization: &str = "";
 const Qualifier: &str = "";
+
+pub const Path_Images: &str = "/images/";
+
+/**
+Save an image to file in the cache directory specific to this application.
+*/
+pub fn cacheImage(platform: String, fileName: String, buffer: &[u8]) -> Result<()>
+{
+	if let Some(dir) = getCacheDir(true)
+	{
+		let path = Path::new(dir.as_str())
+			.join(Path_Images)
+			.join(format!("/{}/", platform.to_lowercase()))
+			.join(fileName);
+		
+		let mut file = File::create(path)
+			.context("")?;
+		file.write_all(buffer)
+			.context("")?;
+		
+		return Ok(());
+	}
+	
+	return Err(error!(ErrorKind::NotFound));
+}
 
 /**
 Get the cache directory specific to this application.
