e0fc153ed0e9d6e30309148f324e18af09215194
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-10-26 01:56:12 -0400

    Update macros.rs
    Added join and joinSep macros, with tests.

--- src/macros.rs
+++ src/macros.rs
@@ -14,3 +14,66 @@ macro_rules! error
 		}
 	}
 }
+
+#[macro_export]
+macro_rules! join
+{
+	($a:expr, $($val:expr),+) => {
+		{
+			let mut s = format!("{}", $a);
+			$( s = format!("{}/{}", s, $val); )+
+			s
+		}
+	}
+}
+
+#[macro_export]
+macro_rules! joinSep
+{
+	($sep:expr, $a:expr, $($val:expr),+) => {
+		{
+			let mut s = format!("{}", $a);
+			$( s = format!("{}{}{}", s, $sep, $val); )+
+			s
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests
+{
+	#[test]
+	fn join()
+	{
+		let a = "1";
+		let b = "z";
+		let c = "what";
+		
+		let r1 = join!(a, b);
+		let r2 = join!(a, b, c);
+		
+		let e1 = String::from("1/z");
+		let e2 = String::from("1/z/what");
+		
+		assert_eq!(e1, r1);
+		assert_eq!(e2, r2);
+	}
+	
+	#[test]
+	fn joinSep()
+	{
+		let separator = "/";
+		let a = "1";
+		let b = "z";
+		let c = "what";
+		
+		let r1 = joinSep!(separator, a, b);
+		let r2 = joinSep!(separator, a, b, c);
+		
+		let e1 = String::from("1/z");
+		let e2 = String::from("1/z/what");
+		
+		assert_eq!(e1, r1);
+		assert_eq!(e2, r2);
+	}
+}
