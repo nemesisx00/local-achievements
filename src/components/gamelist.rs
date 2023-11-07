281c73591eb1f019cf56d607c81d6183b96f7a93
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-07 14:29:06 -0500

    Update gamelist.rs
    Updated GameList component to output a containing div with id.
    
    Added inline property, refresh, to both GameList and Game which controls a custom "refresh" attribute, allowing both components to be forced to redraw at will.
    
    Updated Game component to only output the img tag when the game's icon exists in the local cache.

--- src/components/gamelist.rs
+++ src/components/gamelist.rs
@@ -1,6 +1,7 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+use std::path::Path;
 use ::dioxus::prelude::*;
 use ::fermi::use_atom_ref;
 use crate::join;
@@ -9,24 +10,33 @@ use crate::io::{Path_Games, getImagePath};
 use crate::platforms::steam::SteamApi;
 use crate::state::UserData;
 
-pub fn GameList(cx: Scope) -> Element
+#[inline_props]
+pub fn GameList(cx: Scope, refresh: Option<bool>) -> Element
 {
 	let userData = use_atom_ref(cx, &UserData);
 	
 	let mut games = userData.read().games.clone();
 	games.sort_by(|a, b| a.partial_cmp(b).unwrap());
 	
+	let doRefresh = refresh.is_some_and(|switch| switch == true);
+	
 	return cx.render(rsx!
 	{
-		for game in games.iter()
+		div
 		{
-			Game { game: game.clone(), first: games.first().is_some_and(|f| f == game), hasAchievements: game.achievements.len() > 0 }
+			id: "steamGameList",
+			"refresh": doRefresh,
+			
+			for game in games.iter()
+			{
+				Game { game: game.clone(), first: games.first().is_some_and(|f| f == game), hasAchievements: game.achievements.len() > 0, refresh: doRefresh }
+			}
 		}
 	});
 }
 
 #[inline_props]
-pub fn Game(cx: Scope, game: Game, first: bool, hasAchievements: bool) -> Element
+pub fn Game(cx: Scope, game: Game, first: bool, hasAchievements: bool, refresh: Option<bool>) -> Element
 {
 	let id = match &game.steam
 	{
@@ -40,6 +50,9 @@ pub fn Game(cx: Scope, game: Game, first: bool, hasAchievements: bool) -> Elemen
 		None => String::new(),
 	};
 	
+	let iconExists = !iconPath.is_empty() && Path::new(&iconPath).exists();
+	let doRefresh = refresh.is_some_and(|switch| switch == true);
+	
 	return cx.render(rsx!
 	{
 		if !first
@@ -49,7 +62,18 @@ pub fn Game(cx: Scope, game: Game, first: bool, hasAchievements: bool) -> Elemen
 		
 		div
 		{
-			h3 { img { src: "/{iconPath}", }, "{game.name}", }
+			"appid": "{id}",
+			"refresh": doRefresh,
+			
+			if iconExists
+			{
+				rsx!(h3 { img { src: "/{iconPath}", }, "{game.name}", })
+			}
+			else
+			{
+				rsx!(h3 { "{game.name}", })
+			}
+			
 			hasAchievements.then(|| rsx!{
 				p { "Has achievements" }
 			})
