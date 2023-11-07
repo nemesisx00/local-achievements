0cc0715bf02c1510c296c553d965f399a1191f56
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-07 16:26:11 -0500

    Add recentlyplayedgames.rs
    Added data structures comprising the payload returned from GetRecentlyPlayedGames endpoint.

--- src/platforms/steam/data/mod.rs
+++ src/platforms/steam/data/mod.rs
@@ -7,6 +7,7 @@ mod globalpercentages;
 mod ownedgames;
 mod playerachievements;
 mod playersummaries;
+mod recentlyplayedgames;
 
 pub use auth::AuthData;
 pub use gameschema::GetSchemaForGamePayload;
@@ -14,3 +15,4 @@ pub use globalpercentages::GetGlobalPercentagesPayload;
 pub use ownedgames::{GameInfo, GetOwnedGamesPayload};
 pub use playerachievements::GetPlayerAchievementsPayload;
 pub use playersummaries::GetPlayerSummariesPayload;
+pub use recentlyplayedgames::GetRecentlyPlayedGamesPayload;
