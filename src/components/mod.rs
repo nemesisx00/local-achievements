b77c9bfdfaf935ec4008f079a776c9d41adb3bed
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-24 17:02:06 -0500

    Update mod.rs

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,7 +1,10 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+mod achievement;
+mod achievementlist;
 mod app;
+mod game;
 mod gamelist;
 
 pub use app::App;
