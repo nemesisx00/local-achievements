b9c1ac60fe62d280ee89ce96d265775ec80c8c31
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-23 20:06:37 -0500

    Update main.rs
    Updated to be a bit more specific with launching the application.
    
    Added a crude workaround to be able to load the CSS styles properly when using dx serve; Will need to find a better solution to this at some point.
    
    Added configuration guard to prevent Windows from opening a Command Prompt when starting the application in a Release build.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ and tracking your achievements across multiple platforms in one unified UI.
 
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
+#![cfg_attr(all(windows, not(debug_assertions)), windows_subsystem = "windows")]
 
 mod components;
 mod data;
@@ -13,10 +14,33 @@ mod macros;
 mod platforms;
 mod state;
 
-use ::dioxus_desktop::launch;
+use dioxus_desktop::{WindowBuilder, LogicalSize};
+use ::dioxus_desktop::{Config, launch_cfg};
 use crate::components::App;
+
+pub const AppTitle: &str = "Local Achievements";
+pub const AppVersion: &str = "0.2.0";
+pub const BackgroundColor: (u8, u8, u8, u8) = (26, 26, 26, 255);
+pub const CssFile: &str = "app.css";
+pub const MinimumWindowSize: LogicalSize<u32> = LogicalSize::new(720, 480);
 
 fn main()
 {
-	launch(App);
+	let win = WindowBuilder::default()
+		.with_inner_size(MinimumWindowSize)
+		.with_min_inner_size(MinimumWindowSize)
+		.with_title(format!("{} {}", AppTitle, AppVersion));
+	
+	let css = format!(r#"<link rel="stylesheet" href="{}" />"#, match cfg!(debug_assertions)
+	{
+		true =>  format!("static/{}", CssFile),
+		false => CssFile.to_string(),
+	});
+	
+	let config = Config::default()
+		.with_background_color(BackgroundColor)
+		.with_custom_head(css)
+		.with_window(win);
+	
+	launch_cfg(App, config);
 }
