69f8cac645a55ae4a8139f2b0ec36ffa75698c52
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-24 17:02:02 -0500

    Update gamelist.rs
    Removed Game component, as it was moved into its own file.
    Removed hasAchievements property from usage of Game component.
    Added doc comments.

--- src/components/gamelist.rs
+++ src/components/gamelist.rs
@@ -1,15 +1,20 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
-use std::path::Path;
 use ::dioxus::prelude::*;
 use ::fermi::use_atom_ref;
-use crate::join;
-use crate::data::Game;
-use crate::io::{Path_Games, getImagePath};
-use crate::platforms::steam::SteamApi;
 use crate::state::UserData;
+use super::game::Game;
 
+/**
+Component for displaying the list of games.
+
+---
+
+Property | Description
+---|---
+refresh | An optional boolean property used to force Dioxus to redraw the component.
+*/
 #[inline_props]
 pub fn GameList(cx: Scope, refresh: Option<bool>) -> Element
 {
@@ -29,54 +34,8 @@ pub fn GameList(cx: Scope, refresh: Option<bool>) -> Element
 			
 			for game in games.iter()
 			{
-				Game { game: game.clone(), first: games.first().is_some_and(|f| f == game), hasAchievements: game.achievements.len() > 0, refresh: doRefresh }
+				Game { game: game.clone(), first: games.first().is_some_and(|f| f == game), refresh: doRefresh }
 			}
-		}
-	});
-}
-
-#[inline_props]
-pub fn Game(cx: Scope, game: Game, first: bool, hasAchievements: bool, refresh: Option<bool>) -> Element
-{
-	let id = match &game.steam
-	{
-		Some(info) => info.id,
-		None => 0,
-	};
-	
-	let iconPath = match getImagePath(SteamApi::Platform.into(), join!(Path_Games, id), SteamApi::GameIcon.into())
-	{
-		Some(path) => path,
-		None => String::new(),
-	};
-	
-	let iconExists = !iconPath.is_empty() && Path::new(&iconPath).exists();
-	let doRefresh = refresh.is_some_and(|switch| switch == true);
-	
-	return cx.render(rsx!
-	{
-		if !first
-		{
-			rsx!(hr {})
-		}
-		
-		div
-		{
-			"appid": "{id}",
-			"refresh": doRefresh,
-			
-			if iconExists
-			{
-				rsx!(h3 { img { src: "/{iconPath}", }, "{game.name}", })
-			}
-			else
-			{
-				rsx!(h3 { "{game.name}", })
-			}
-			
-			hasAchievements.then(|| rsx!{
-				p { "Has achievements" }
-			})
 		}
 	});
 }
