c99a3b7c8d1205805730312a46541778d86a0ccd
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-24 16:50:08 -0500

    Update gameschema.rs
    Added getAchievements to GetSchemaForGamePayload as a convenient way to get the full list of crate::data::Achievement instances ready to be used in the data layer.

--- src/platforms/steam/data/gameschema.rs
+++ src/platforms/steam/data/gameschema.rs
@@ -2,6 +2,8 @@
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
 use ::serde::{Deserialize, Serialize};
+use crate::data::{Achievement, PlatformInfo};
+use crate::platforms::Platform;
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
 pub struct GameAchievement
@@ -37,6 +39,36 @@ The expected response data returned by the GetSchemaForGame endpoint.
 pub struct GetSchemaForGamePayload
 {
 	pub game: GameSchema,
+}
+
+impl GetSchemaForGamePayload
+{
+	pub fn getAchievements(&self) -> Vec<Achievement>
+	{
+		let mut achievements = vec![];
+		if let Some(a) = &self.game.availableGameStats.achievements
+		{
+			for ga in a
+			{
+				let description = match ga.description.to_owned()
+				{
+					Some(d) => d,
+					None => String::default(),
+				};
+				
+				let mut pi = PlatformInfo::new(ga.name.to_owned(), ga.displayName.to_owned(), description, Platform::Steam);
+				pi.icon = Some(ga.icon.to_owned());
+				pi.iconLocked = Some(ga.icongray.to_owned());
+				
+				let mut achievement = Achievement::default();
+				achievement.platforms.push(pi);
+				
+				achievements.push(achievement);
+			}
+		}
+		
+		return achievements;
+	}
 }
 
 #[derive(Clone, Debug, Default, Deserialize, PartialEq, Serialize)]
