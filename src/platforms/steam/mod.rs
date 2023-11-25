160c0ea4128fba6c442eedd7b806314db8c34798
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-24 21:57:09 -0500

    Update mod.rs
    Re-aliased GameAchievement as SteamAchievementMetadata and aliased PlayerAchievement as SteamAchievement.

--- src/platforms/steam/mod.rs
+++ src/platforms/steam/mod.rs
@@ -4,5 +4,10 @@
 mod api;
 mod data;
 
-pub use data::{AuthData as SteamAuth, GameAchievement as SteamAchievement, GameInfo as SteamGame};
+pub use data::{
+	AuthData as SteamAuth,
+	GameAchievement as SteamAchievementMetadata,
+	GameInfo as SteamGame,
+	PlayerAchievement as SteamAchievement,
+};
 pub use api::Api as SteamApi;
