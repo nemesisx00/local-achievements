f9b85ea11401f2f8c7bdf4bbf1146017cdd89c4d
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-24 19:42:21 -0500

    Update mod.rs

--- src/platforms/steam/data/mod.rs
+++ src/platforms/steam/data/mod.rs
@@ -10,7 +10,7 @@ mod playersummaries;
 mod recentlyplayedgames;
 
 pub use auth::AuthData;
-pub use gameschema::GetSchemaForGamePayload;
+pub use gameschema::{GameAchievement, GetSchemaForGamePayload};
 pub use globalpercentages::GetGlobalPercentagesPayload;
 pub use ownedgames::{GameInfo, GetOwnedGamesPayload};
 pub use playerachievements::GetPlayerAchievementsPayload;
