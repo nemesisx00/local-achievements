4fe4c316f2f8af99e66dea0faa5de493010872b9
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-12-04 16:49:46 -0500

    Update mod.rs
    Added function freeChildren; It's a bit verbose to do manually but also useful enough to reuse later.

--- rust/src/nodes/mod.rs
+++ rust/src/nodes/mod.rs
@@ -1,3 +1,27 @@
 mod retroachievement;
 mod steamachievement;
 mod game;
+
+use ::godot::engine::Node;
+use ::godot::obj::Gd;
+
+/**
+Iterate over a node's children, calling queue_free() on each of them.
+
+Parameter | Description
+---|---
+node | The `Node` whose children are to be freed.
+*/
+pub fn freeChildren(node: &mut Gd<Node>)
+{
+	if node.get_child_count() > 0
+	{
+		for i in (0..node.get_child_count()).rev()
+		{
+			if let Some(c) = node.get_child(i).as_mut()
+			{
+				c.queue_free();
+			}
+		}
+	}
+}
