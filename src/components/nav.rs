913f01966f68c5db82b9aa9039775289e9a9605c
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2026-04-09 19:32:37 -0400

    Update nav element
    Added some local assets making the nav element feel more uniform as well as slimming down a little bit.
    Updated the profile and settings buttons to use the custom IconButton element.

--- src/components/nav.rs
+++ src/components/nav.rs
@@ -1,11 +1,9 @@
+use std::path::PathBuf;
+
 use freya::icons::lucide;
-use freya::prelude::{AccessibilityExt, Alignment, Border, BorderAlignment,
-	BorderWidth, Button, ButtonLayoutThemePartialExt, ChildrenExt,
-	CircularLoader, ContainerExt, ContainerSizeExt, ContainerWithContentExt,
-	Direction, Element, Gaps, IntoElement, Layer, LayerExt, Position, Size,
-	StyleExt, TextAlign, TextStyleExt, WritableUtils, label, rect, svg,
-	use_side_effect, use_state};
+use freya::prelude::{AccessibilityExt, Alignment, Border, BorderAlignment, BorderWidth, Button, ButtonLayoutThemePartialExt, ChildrenExt, CircularLoader, ContainerExt, ContainerSizeExt, ContainerWithContentExt, Direction, Element, Event, EventHandler, Gaps, ImageViewer, IntoElement, Layer, LayerExt, Position, PressEventData, Size, StyleExt, TextAlign, TextStyleExt, WritableUtils, label, rect, svg, use_side_effect, use_state};
 use freya::radio::use_radio;
+use crate::components::IconButton;
 use crate::components::profile::{ProfileElement, ProfileState};
 use crate::constants::{BackgroundColor, BorderColor, TextColor};
 use crate::data::radio::{AppDataChannel, DataChannel};
@@ -53,6 +51,13 @@ pub fn NavBar() -> impl IntoElement
 		_ => None,
 	};
 	
+	let profileIcon = match profileState.read().clone() == ProfileState::Hiding
+		|| profileState.read().clone() == ProfileState::Shown
+	{
+		false => lucide::chevron_right(),
+		true => lucide::chevron_left(),
+	};
+	
 	use_side_effect(move || **activeContent.write() = Some(selected()));
 	
 	return rect()
@@ -61,13 +66,11 @@ pub fn NavBar() -> impl IntoElement
 		.height(Size::Fill)
 		.layer(Layer::Overlay)
 		.margin(Gaps::new(0.0, 0.0, 0.0, 7.5))
-		.max_width(Size::px(100.0))
-		.min_width(Size::px(50.0))
 		.position(Position::new_absolute()
 			.left(0.0)
 			.top(0.0)
 		)
-		.width(Size::FillMinimum)
+		.width(Size::px(60.0))
 		
 		.child(
 			rect()
@@ -85,15 +88,20 @@ pub fn NavBar() -> impl IntoElement
 				))
 				.direction(Direction::Vertical)
 				.height(Size::Fill)
-				.padding(Gaps::new(10.0, 10.0, 5.0, 0.0))
-				.spacing(10.0)
+				.padding(Gaps::new(10.0, 5.0, 5.0, 0.0))
+				.spacing(5.0)
 				.width(Size::FillMinimum)
 				
 				// Open/close button
 				.child(
-					Button::new()
-						.width(Size::Fill)
-						.on_press(move |_| {
+					IconButton::new(profileIcon)
+						.alt("Show Profiles")
+						.color(TextColor)
+						.height(Size::px(48.0))
+						.innerHeight(Size::px(32.0))
+						.innerWidth(Size::px(32.0))
+						.width(Size::px(48.0))
+						.onPress(move |_| {
 							let mut profileState = profileState.write();
 							match profileState.clone()
 							{
@@ -102,87 +110,93 @@ pub fn NavBar() -> impl IntoElement
 								_ => {}
 							}
 						})
-						
-						.maybe_child((
-								profileState.read().clone() == ProfileState::Hiding
-								|| profileState.read().clone() == ProfileState::Shown
-							)
-							.then(||
-								svg(lucide::chevron_left())
-									.color(TextColor)
-									.height(Size::px(32.0))
-									.width(Size::px(32.0))
-							))
-						
-						.maybe_child((
-								profileState.read().clone() == ProfileState::Hidden
-								|| profileState.read().clone() == ProfileState::Showing
-							)
-							.then(||
-								svg(lucide::chevron_right())
-									.color(TextColor)
-									.height(Size::px(32.0))
-									.width(Size::px(32.0))
-							))
+				)
+				
+				.child(
+					IconButton::new(lucide::settings())
+						.alt("Settings")
+						.color(TextColor)
+						.height(Size::px(48.0))
+						.innerHeight(Size::px(32.0))
+						.innerWidth(Size::px(32.0))
+						.width(Size::px(48.0))
+						.onPress(move |_| selected.set(ActiveContent::Settings))
+				)
+				
+				.child(
+					navButton(
+						"assets/battlenet-logo.png",
+						"Battle.Net",
+						move |_| selected.set(ActiveContent::BattleNet)
+					)
+				)
+				
+				.child(
+					navButton(
+						"assets/egs-logo.png",
+						"Epic Games Store",
+						move |_| selected.set(ActiveContent::EpicGamesStore)
+					)
+				)
+				
+				.child(
+					navButton(
+						"assets/gog-logo.png",
+						"GOG",
+						move |_| selected.set(ActiveContent::Gog)
+					)
 				)
 				
 				.child(
 					Button::new()
-						.width(Size::percent(100.0))
-						.on_press(move |_| selected.set(ActiveContent::Settings))
+						.height(Size::px(48.0))
+						.margin(Gaps::new_all(0.0))
+						.width(Size::px(48.0))
 						.child(
-							svg(lucide::settings())
-								.color(TextColor)
-								.height(Size::px(32.0))
+							ImageViewer::new(PathBuf::from("assets/ra-logo.png"))
+								.a11y_alt("Retro Achievements")
 								.width(Size::px(32.0))
-								.a11y_alt("Settings")
 						)
-				)
-				
-				.child(
-					Button::new()
-						.width(Size::percent(100.0))
-						.child("BNet")
-						.on_press(move |_| selected.set(ActiveContent::BattleNet))
-				)
-				
-				.child(
-					Button::new()
-						.width(Size::percent(100.0))
-						.child("EGS")
-						.on_press(move |_| selected.set(ActiveContent::EpicGamesStore))
-				)
-				
-				.child(
-					Button::new()
-						.width(Size::percent(100.0))
-						.child("GOG")
-						.on_press(move |_| selected.set(ActiveContent::Gog))
-				)
-				
-				.child(
-					Button::new()
-						.width(Size::percent(100.0))
-						.child("RA")
 						.on_press(move |_| selected.set(ActiveContent::RetroAchievements))
 				)
 				
 				.child(
-					Button::new()
-						.width(Size::percent(100.0))
-						.child("RPCS3")
-						.on_press(move |_| selected.set(ActiveContent::Rpcs3))
+					navButton(
+						"assets/rpcs3-logo.png",
+						"RPCS3",
+						move |_| selected.set(ActiveContent::Rpcs3)
+					)
 				)
 				
 				.child(
-					Button::new()
-						.width(Size::percent(100.0))
-						.child("Steam")
-						.on_press(move |_| selected.set(ActiveContent::Steam))
+					navButton(
+						"assets/steam-logo.png",
+						"Steam",
+						move |_| selected.set(ActiveContent::Steam)
+					)
 				)
 				
 				.maybe_child(taskCounter)
 		)
 		
 		.child(ProfileElement::new());
+}
+
+fn navButton(
+	path: &'static str,
+	alt: &'static str,
+	pressHandler: impl Into<EventHandler<Event<PressEventData>>>
+) -> impl IntoElement
+{
+	return Button::new()
+		.height(Size::px(48.0))
+		.margin(Gaps::new_all(0.0))
+		.width(Size::px(48.0))
+		.child(
+			ImageViewer::new(PathBuf::from(path))
+				.a11y_alt(alt)
+				.height(Size::px(32.0))
+				.width(Size::px(32.0))
+		)
+		.on_press(pressHandler);
 }
