fdc68adcdf91d15b0d375a964f4aa3d45d47d81b
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2026-04-12 16:22:23 -0400

    Reorganized the files for extensions in reliquarian-components

---
+++ crates/reliquarian-components/src/extensions/mod.rs
@@ -0,0 +1,3 @@
+mod pressable;
+
+pub use pressable::PressableExt;
---
+++ crates/reliquarian-components/src/extensions/pressable.rs
@@ -0,0 +1,81 @@
+use freya::{prelude::{CursorIcon, Event, EventHandler, EventHandlersExt, Label,
+	Platform, PressEventData, Rect, WinitPlatformExt}, radio::Writable};
+
+pub trait PressableExt
+	where Self: EventHandlersExt,
+{
+	/**
+	Handle press events and also set the cursor to `CursorIcon::Pointer` while
+	the pointer is over the element.
+	*/
+	fn pressable(self, handler: impl Into<EventHandler<Event<PressEventData>>>) -> Self
+	{
+		let handler = handler.into();
+		return self.on_pointer_enter(move |_| {
+			Platform::get().with_window(
+				None,
+				move |window| window.set_cursor(CursorIcon::Pointer)
+			);
+		})
+		
+		.on_pointer_leave(move |_| {
+			Platform::get().with_window(
+				None,
+				move |window| window.set_cursor(CursorIcon::default())
+			);
+		})
+		
+		.on_press(move |e| {
+			Platform::get().with_window(
+				None,
+				move |window| window.set_cursor(CursorIcon::default())
+			);
+			handler.call(e);
+		});
+	}
+	
+	/**
+	Handle press events and also set the cursor to `CursorIcon::Pointer` while
+	the pointer is over the element.
+	
+	Also accepts a `Writable<bool>` to signal the state of the pointer on enter
+	and leave.
+	*/
+	fn pressableWithHover(
+		self,
+		hovering: impl Into<Writable<bool>>,
+		handler: impl Into<EventHandler<Event<PressEventData>>>
+	) -> Self
+	{
+		let handler = handler.into();
+		let mut hovering = hovering.into();
+		let mut hoveringLeave = hovering.clone();
+		
+		return self.on_pointer_enter(move |_| {
+			Platform::get().with_window(
+				None,
+				move |window| window.set_cursor(CursorIcon::Pointer)
+			);
+			*hovering.write() = true;
+		})
+		
+		.on_pointer_leave(move |_| {
+			Platform::get().with_window(
+				None,
+				move |window| window.set_cursor(CursorIcon::default())
+			);
+			*hoveringLeave.write() = false;
+		})
+		
+		.on_press(move |e| {
+			Platform::get().with_window(
+				None,
+				move |window| window.set_cursor(CursorIcon::default())
+			);
+			handler.call(e);
+		});
+	}
+}
+
+impl PressableExt for Label {}
+impl PressableExt for Rect {}
